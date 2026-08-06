0d0b5927d246d1674c7386999be282349cc4f2cf
pankajsinghhh <ace847714@gmail.com>
2026-08-06 19:37:58 +0530

    commited some changes

--- cses/Company Queries I.cpp
+++ cses/Company Queries I.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+const int LOG = 31;
 
 int main(){
     ios::sync_with_stdio(false);
@@ -8,9 +9,6 @@ int main(){
 
     int n, q; 
     cin >> n >> q;
-    
-    int LOG = 0;
-    while((1<<LOG) <= n) LOG++;
 
     vector<vector<int>> up(n+1, vector<int>(LOG, 0));
 
@@ -18,38 +16,22 @@ int main(){
         cin >> up[i][0];
     }
 
-    for(int j= 1; j < LOG; j++){
+    for(int j = 1; j < LOG; j++){
         for(int i = 1; i <= n; i++){
             up[i][j] = up[up[i][j-1]][j-1];
         }
     }
 
     while(q--){
-        int x, k;
+        int x; long long k;
         cin >> x >> k;
 
-
+        for(int j = 0; j < LOG; j++){
+            if(k & (1LL<<j)){
+                x = up[x][j];
+                 if (x == 0) break;
+            }
+        }
+        cout << (x == 0 ? -1 : x) << '\n';
     }
-    
 }
-
-
-
-// vector<int> boss(n+1, -1);
-// for(int i = 2; i <= n; i++){
-//     cin >> boss[i];
-// }
-// vector<vector<int>> bosslvl(n+1, vector<int>(n, -1));
-// for(int i = 2;i <= n; i++) bosslvl[i][1] = boss[i];
-
-// for(int i = 2;i <= n; i++){
-//     for(int j = 2; j < n; j++){
-//         bosslvl[i][j] = (bosslvl[i][j-1] != -1)? boss[bosslvl[i][j-1]] : -1;
-//     }
-// }
-// while(q--){
-//     int x , k;
-//     cin >> x >> k;
-    
-//     cout<< bosslvl[x][k] << "\n";
-// }
